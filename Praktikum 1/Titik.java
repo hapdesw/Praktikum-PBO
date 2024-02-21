b39e0336c288ac35ecfe8c57cf67fa1bde35b2ac
Happy Desita <124116194+hapdesw@users.noreply.github.com>
2024-02-21 11:47:30 +0700

    Menambahkan overloading

--- Praktikum 1/Titik.java
+++ Praktikum 1/Titik.java
@@ -1,31 +1,34 @@
-public class Titik{
+public class MTitik{
+  public static void main(String[] args){
+    Titik t1;
+    Titik t2;
+    Titik t3;
+
+    t1 = new Titik();
+    t2 = new Titik();
+    t3 = new Titik(9, 3);
+
+    t1.setAbsis(10);
+    t1.setOrdinat(8);
+    t2.setAbsis(4);
+    t2.setOrdinat(6);
+
+    float counterTitik;
     float absis;
     float ordinat;
-    static float counterTitik;
 
-    Titik(){
-        absis = 0;
-        ordinat = 0;
-        counterTitik++;
-    }
+    counterTitik = Titik.getCounterTitik();
+    System.out.println("Jumlah objek titik: " + counterTitik);
 
-    void setAbsis(float a){
-        absis = a;
-    }
+    absis = t1.getAbsis();
+    ordinat = t1.getOrdinat();
+    System.out.printf("t1(%.1f, %.1f)\n", absis, ordinat);
 
-    void setOrdinat(float o){
-        ordinat = o;
-    }
+    absis = t2.getAbsis();
+    ordinat = t2.getOrdinat();
+    System.out.printf("t2(%.1f, %.1f)\n", absis, ordinat);
 
-    float getAbsis(){
-        return absis;
-    }
+    System.out.printf("t3(%.1f, %.1f)\n", t3.getAbsis(), t3.getOrdinat());
 
-    float getOrdinat(){
-        return ordinat;
-    }
-
-    static float getCounterTitik(){
-        return counterTitik;
-    }
+  }
 }
